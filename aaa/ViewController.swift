a7ff110ecafe3372e75f12a1bbcfd5807107b6aa
thomas <xucanhao@gmail.com>
2020-03-23 21:29:30 +0800

    Initial Commit

---
+++ aaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aaa
+//
+//  Created by Thomas Canhao Xu on 2020/3/23.
+//  Copyright © 2020 Thomas Canhao Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
