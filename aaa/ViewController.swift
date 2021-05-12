fbbd461b8930f64219ab62116adc673509ce7d27
Shanis Cato <shanis.cato@outlook.com>
2021-05-12 10:38:55 -0700

    Initial commit: UITableView configured

--- aaa/ViewController.swift
+++ aaa/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var grid: UITableView!
+
+    var audioPlayer = AVAudioPlayer()
+    
+    var sounds = [Sound]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        grid.delegate = self
+        grid.dataSource = self
+        
+//        let sound = Bundle.main.path(forResource: "mysound", ofType: "mp3")
+//
+//        do {
+//            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+//        }
+//        catch{
+//            //print{error}
+//        }
+    }
+    func configureSounds(){
+        sounds.append(Sound(name: "bang"))
+        sounds.append(Sound(name:"shocked"))
     }
 
-
+    @IBAction func onClickPlaySound(_ sender: Any) {
+        audioPlayer.play()
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let sound = sounds[indexPath.row]
+        cell.textLabel?.text = sound.name
+        cell.textLabel?.font = UIFont(name:"Helvetica-Bold", size:18)
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //curent sound
+        let position = indexPath.row
+        //beats
+        guard let vc = storyboard?.instantiateViewController(identifier: "player")
+        else{
+            return
+        }
+        present(vc, animated:true)
+    }
 }
-
+struct Sound {
+    let name: String
+    
+}
